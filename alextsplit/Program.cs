3cb5eb3bc6c419aed251d9a1a83928f0d51f5837
Lindokuhle Ndaba <lindokuhle.ndaba@chillisoft.co.za>
2020-04-15 15:15:58 +0200

    chore: initial project commit

---
+++ alextsplit/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.CommandLine;
+using System.CommandLine.Invocation;
+using System.IO;
+
+namespace alextsplit
+{
+    class Program
+    {
+        private static readonly Script Script = new Script();
+        static void Main(string[] args)
+        {
+            var rootCommand = CreateOptions();
+
+            rootCommand.Name = "alextsplit";
+
+            rootCommand.Handler =
+                CommandHandler.Create<string, int>(ExecuteFileAndReturnExitCode);
+
+            rootCommand.InvokeAsync(args).Wait();
+        }
+
+        private static RootCommand CreateOptions()
+        {
+            RootCommand rootCommand = new RootCommand(
+                description: "Splits a log file into small text files with the specified number of lines each.");
+            Option nameOption = new Option<string>(
+                aliases: new string[] { "--name", "-n" }
+                , description: "The log file path to be processed.");
+            rootCommand.AddOption(nameOption);
+
+            Option linesOption = new Option<int>(
+                aliases: new string[] { "--lines", "-l" }
+                , description: "The number of lines to write to each file." +
+                               "The default number of lines if not provided is 2000",
+                getDefaultValue: () => 2000);
+            rootCommand.AddOption(linesOption);
+
+            return rootCommand;
+        }
+
+        private static int ExecuteFileAndReturnExitCode(string name, int lines)
+        {
+            if (!File.Exists(name))
+            {
+
+                Console.WriteLine($"option {name} not recognized");
+                Console.WriteLine("Run 'alextsplit  --help' for more information on a command..");
+
+            }
+            else
+            {
+                Script.ExecuteFile(name, lines);
+            }
+
+            return 0;
+        }
+
+    }
+}
---
+++ alextsplit/Script.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Reflection;
+using log4net;
+using log4net.Config;
+
+namespace alextsplit
+{
+    public class Script
+    {
+        private static readonly ILog _log = LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
+
+        public Script()
+        {
+            LoadConfiguration();
+        }
+        public void ExecuteFile(string fileName, int numberOfLines)
+        {
+            var currentDirectory = fileName;
+            var fileNameWithoutExtension = Path.GetFileNameWithoutExtension(currentDirectory);
+
+            var logFolder = CreateLogFolder(currentDirectory);
+            var outFileName = logFolder + "\\" + fileNameWithoutExtension + "_{0}.txt";
+            var outFileNumber = 1;
+
+            var reader = File.OpenText(currentDirectory);
+            try
+            {
+                var stopwatch = new Stopwatch();
+                stopwatch.Start();
+                while (!reader.EndOfStream)
+                {
+                    var writer = File.CreateText(string.Format(outFileName, outFileNumber++));
+                    for (int counter = 0; counter < numberOfLines; counter++)
+                    {
+                        _log.Info($"Processing {counter} into file {outFileNumber}");
+                        writer.WriteLine(reader.ReadLine());
+                        if (reader.EndOfStream) break;
+                    }
+                    writer.Close();
+
+                }
+                reader.Close();
+                stopwatch.Stop();
+                _log.Info($"Splitting complete after {stopwatch.Elapsed.TotalSeconds} seconds");
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine(exception);
+            }
+        }
+
+        private static string CreateLogFolder(string filepath)
+        {
+            var logFolder = filepath.Replace(".", "_");
+
+            if (!Directory.Exists(logFolder))
+                Directory.CreateDirectory(logFolder);
+
+            return logFolder;
+
+        }
+
+        public void LoadConfiguration()
+        {
+            var logRepository = LogManager.GetRepository(Assembly.GetEntryAssembly());
+            XmlConfigurator.Configure(logRepository, new FileInfo("log4net.config"));
+        }
+    }
+}
